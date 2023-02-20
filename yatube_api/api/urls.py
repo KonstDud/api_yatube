8c351f20c343321143d388dd0c69176bb9b9f2a8
Dmitry <korkanovi4@gmail.com>
2023-02-21 01:42:05 +0300

    fix imports

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -7,7 +7,9 @@ from .views import CommentViewSet, GroupViewSet, PostViewSet
 router = routers.DefaultRouter()
 router.register("posts", PostViewSet, basename="post")
 router.register("groups", GroupViewSet, basename="group")
-router.register(r"posts/(?P<post_id>\d+)/comments", CommentViewSet, basename="comment")
+router.register(
+    r"posts/(?P<post_id>\d+)/comments", CommentViewSet, basename="comment"
+)
 
 urlpatterns = [
     path("v1/", include(router.urls)),
